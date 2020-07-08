684b9e4753426d02b4fd97b19250e9c1a5329baf
61-الدانة سعد الرشيدي <aldanaalrashidi0@gmail.com>
2020-07-08 19:37:52 +0300

    Done

---
+++ IOSClasswork8/iOSClasswork8/ViewController/WeatherCell.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherCell.swift
+//  iOSClaswork8
+//
+//  Created by tahani alrashidi on 7/8/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    
+    @IBOutlet var cardImage: UIImageView!
+    @IBOutlet var cardLabel: UILabel!
+    
+  
+
+}
